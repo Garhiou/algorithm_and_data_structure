614815c3ca7f14c9e35adddd27aa769798ba98cc
Ibrahim Garhiou <s3541266@stud.uni-frankfurt.de>
2025-09-24 12:21:25 +0200

    blind seach

---
+++ blind_search.py
@@ -0,0 +1,9 @@
+"""
+Blind search is uninformed search; only the search graph is (implicitly) given, and there is no other information such as heuristics.
+Input:
+Set of initial nodes
+Set of target nodes or unique definition of the properties of the target nodes
+Successor function N
+Output:
+Path to the target node, if it exists
+"""
