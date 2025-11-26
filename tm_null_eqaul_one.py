2f2d88b07cc7bdc1ccfba208f92d8fda0a35fc4c
Ibrahim Garhiou <s3541266@stud.uni-frankfurt.de>
2025-11-26 16:04:32 +0100

    check with class

--- tm_null_eqaul_one.py
+++ tm_null_eqaul_one.py
@@ -1,4 +1,5 @@
 def tm_null_equal_one(word: str) -> bool:
+
     count_0 = 0
     count_1 = 0
     for char in word:
@@ -6,15 +7,50 @@ def tm_null_equal_one(word: str) -> bool:
             count_0 += 1
         elif char == "1":
             count_1 += 1
-        else: 
+        else:
             return False
     return count_0 == count_1
 
+
+class TMNullEqualOne:
+    
+
+    def __init__(self):
+       
+        self.count_0 = 0
+        self.count_1 = 0
+
+    def check(self, word: str) -> bool:
+       
+        self.count_0 = 0
+        self.count_1 = 0
+
+        for char in word:
+            if char == "0":
+                self.count_0 += 1
+            elif char == "1":
+                self.count_1 += 1
+            else:
+                return False
+        return self.count_0 == self.count_1
+
+
+# Tests
 word1 = "0011"     # -> True
 word2 = "010101"   # -> True
 word3 = "001111"   # -> False
-word4 = "00211"    # -> False 
-words = ["0011", "010101", "001111", "00211"]
+word4 = "00211"    # -> False
+
+words = [word1, word2, word3, word4]
+
+tm = TMNullEqualOne()
+
+print("Class")
+for w in words:
+    print(w, "->", tm.check(w))
+
+print("-----------------------------------")
+
+print("Function")
 for w in words:
     print(w, "->", tm_null_equal_one(w))
-
